d49a80cc5c28dc9f334a4ce28ad797d0dbce4156
Karelle Hofler <karellehofler@gmail.com>
2022-05-25 04:16:55 -0400

    Spring boot setup

---
+++ src/main/java/com/gardenmanagementserver/GardenManagementServerApplication.java
@@ -0,0 +1,13 @@
+package com.gardenmanagementserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardenManagementServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GardenManagementServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gardenmanagementserver/GardenManagementServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gardenmanagementserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GardenManagementServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
